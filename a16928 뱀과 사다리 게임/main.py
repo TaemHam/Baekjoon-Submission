4ff4bb42f9487c7e9c92cf5ba429bc6123832973
TaemHam <95671168+TaemHam@users.noreply.github.com>
2021-12-14 08:27:16 +0900

    2021-12-14 Upload

---
+++ a16928 뱀과 사다리 게임/main.py
@@ -0,0 +1,117 @@
+# CP template Version 1.006
+import os
+import sys
+#import string
+#from functools import cmp_to_key, reduce, partial
+#import itertools
+#from itertools import product
+#import collections
+#from collections import deque
+#from collections import Counter, defaultdict as dd
+#import math
+#from math import log, log2, ceil, floor, gcd, sqrt
+#from heapq import heappush, heappop
+#import bisect
+#from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ######## INPUT AREA BEGIN ##########
+    INF = 101
+    input = sys.stdin.readline
+    n = sum(map(int,input().split()))
+    l = [False] * INF
+
+    for _ in range(n):
+        i,j = map(int,input().split())
+        l[j] = i
+
+    m = [0, 0, 1, 1, 1, 1, 1, 1]
+
+    for i in range(8,101):
+        q = []
+        for j in range(1,7):
+            q.append(m[i-j] + 1)
+            if l[i-j] != False:
+                if l[i-j] < i-j:
+                    q[j-1] = m[l[i-j]] + 1
+            try:
+                if l.index(i-j) < i-j:
+                    q[j-1] = INF
+            except:
+                None
+
+        m.append(min(q))
+    
+    print(m[100])
+
+
+    # ######## INPUT AREA END ############
+
+
+# TEMPLATE ###############################
+
+
+enu = enumerate
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def pr(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
---
+++ a2579 계단 오르기/main.py
@@ -0,0 +1,117 @@
+# CP template Version 1.006
+import os
+import sys
+#import string
+#from functools import cmp_to_key, reduce, partial
+#import itertools
+#from itertools import product
+#import collections
+#from collections import deque
+#from collections import Counter, defaultdict as dd
+#import math
+#from math import log, log2, ceil, floor, gcd, sqrt
+#from heapq import heappush, heappop
+#import bisect
+#from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ######## INPUT AREA BEGIN ##########
+
+    input = sys.stdin.readline
+    n = int(input().strip())
+
+    steps = []
+    for _ in range(n):
+        steps.append(int(input().strip()))
+
+    d = []
+    d.append(steps[0])
+    if n == 1:
+        print(d[0])
+        return
+    d.append(max(d[0] + steps[1], steps[1]))
+    if n == 2:
+        print(d[1])
+        return
+    d.append(max(d[0] + steps[2], steps[1] + steps[2]))
+    if n == 3:
+        print(d[2])
+        return
+
+    for i in range(3,n):
+        d.append(max((d[i-2] + steps[i]), (d[i-3] + steps[i-1] + steps[i])))
+    
+    print(d.pop())
+
+
+
+    # ######## INPUT AREA END ############
+
+
+# TEMPLATE ###############################
+
+
+enu = enumerate
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def pr(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
