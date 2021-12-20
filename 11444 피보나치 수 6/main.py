3c289770e7935b18837740677ec508d4f9d30348
TaemHam <95671168+TaemHam@users.noreply.github.com>
2021-12-20 20:18:57 +0900

    2021-12-20 Upload

---
+++ 11444 피보나치 수 6/main.py
@@ -0,0 +1,132 @@
+# CP template Version 1.006
+import os
+import sys
+#import string
+#from functools import cmp_to_key, reduce, partial
+#import itertools
+#from itertools import product
+#import collections
+from collections import deque
+#from collections import Counter, defaultdict as dd
+#import math
+#from math import log, log2, ceil, floor, gcd, sqrt
+#from heapq import heappush, heappop
+#import bisect
+#from bisect import bisect_left as bl, bisect_right as br
+DEBUG = False
+
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ######## INPUT AREA BEGIN ##########
+
+    input = sys.stdin.readline
+    n = int(input().strip())
+    o, a, t = [[1,1],[1,0]], [[1,1],[1,0]], [[1,1],[1,0]]
+    mod = int(1e9)+7
+
+    def odd(a):
+        t[0][0] = (a[0][0] * o[0][0] + a[0][1] * o[1][0]) % mod
+        t[1][0] = (a[0][0] * o[0][1] + a[0][1] * o[1][1]) % mod
+        t[0][1] = (a[1][0] * o[0][0] + a[1][1] * o[1][0]) % mod
+        t[1][1] = (a[1][0] * o[0][1] + a[1][1] * o[1][1]) % mod
+        for i in range(2):
+            for j in range(2):
+                a[i][j] = t[i][j]
+        return (a)
+
+    def even(a):
+        t[0][0] = (a[0][0] * a[0][0] + a[0][1] * a[1][0]) % mod
+        t[1][0] = (a[0][0] * a[0][1] + a[0][1] * a[1][1]) % mod
+        t[0][1] = (a[1][0] * a[0][0] + a[1][1] * a[1][0]) % mod
+        t[1][1] = (a[1][0] * a[0][1] + a[1][1] * a[1][1]) % mod
+        for i in range(2):
+            for j in range(2):
+                a[i][j] = t[i][j]
+        return (a)
+
+    def dac(s,b):
+        if b == 1:
+            return s
+
+        a = dac(s,b//2)
+
+        c = even(a)
+
+        if b % 2:
+            result = odd(c)
+        else:
+            result = c
+
+        return result
+
+    print(dac(a,n)[0][1])
+        
+
+    # ######## INPUT AREA END ############
+
+
+# TEMPLATE ###############################
+
+
+enu = enumerate
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = sys.stdin.readline  # by default
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def pr(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
